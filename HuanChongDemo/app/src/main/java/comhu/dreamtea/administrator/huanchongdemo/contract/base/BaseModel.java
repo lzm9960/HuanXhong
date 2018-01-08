c5d235bfa8aa012f01a199a8d2f3d317b2d68837
DreamTea2 <2919970767@qq.com>
2018-01-08 11:42:39 +0800

    in

--- HuanChongDemo/app/src/main/java/comhu/dreamtea/administrator/huanchongdemo/contract/base/BaseModel.java
+++
@@ -1,8 +0,0 @@
-package comhu.dreamtea.administrator.huanchongdemo.contract.base;
-
-/**
- * Created by Administrator on 2018/1/8.
- */
-
-public interface BaseModel {
-}
--- HuanChongDemo/app/src/main/java/comhu/dreamtea/administrator/huanchongdemo/contract/cal/Callbacks.java
+++
@@ -1,9 +0,0 @@
-package comhu.dreamtea.administrator.huanchongdemo.contract.cal;
-
-/**
- * Created by Administrator on 2018/1/8.
- */
-
-public  interface Callbacks {
-     void  setCall(String call);
-}
